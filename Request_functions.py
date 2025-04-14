35ef0f1f400f8a66b92ba8d7f3bc639366f876a1
Basil Benny <85282801+basilbenny1002@users.noreply.github.com>
2025-04-14 16:08:22 +0530

    Add files via upload

---
+++ Request_functions.py
@@ -0,0 +1,24 @@
+from typing import Union
+from pydantic import BaseModel
+from fastapi.responses import JSONResponse
+from fastapi import FastAPI, Query
+from fastapi import FastAPI
+app = FastAPI()
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+def start_scraper(*args):
+    for i in args:
+        print(i)
+    return {"time_remaining": "50240"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+@app.get("/Twitch_scraper")
+def run_Scraper(category: str, minimum_followers: Union[int, None] = Query(default=None), language: Union[str, None] = Query(default=None), viewer_count: Union[str, None] = Query(default=None) ):
+    data = start_scraper(category, minimum_followers, language, viewer_count)
+    return JSONResponse(status_code=200, content=data)
+
+
