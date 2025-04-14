62f6a1eac49ff8578d68c81d8a6a68a967a944c4
Basil Benny <85282801+basilbenny1002@users.noreply.github.com>
2025-04-15 00:06:26 +0530

    Update Request_functions.py

--- Request_functions.py
+++ Request_functions.py
@@ -8,17 +8,18 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-def start_scraper(*args):
-    for i in args:
-        print(i)
-    return {"time_remaining": "50240"}
+def start_scraper(**kwargs):
+    data = dict(kwargs)
+    data["time_remaining"]= "50240"
+    return data
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 @app.get("/Twitch_scraper")
 def run_Scraper(category: str, minimum_followers: Union[int, None] = Query(default=None), language: Union[str, None] = Query(default=None), viewer_count: Union[str, None] = Query(default=None) ):
-    data = start_scraper(category, minimum_followers, language, viewer_count)
+    data = start_scraper(category=category, minimum_followers=minimum_followers, language=language , viewer_count=viewer_count)
     return JSONResponse(status_code=200, content=data)
+
 
 
