be43af4a83ea5e4a2483f22f6711af944b85a322
Basil Benny <85282801+basilbenny1002@users.noreply.github.com>
2025-04-26 23:04:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ ANYT = AnyValue(choice=True)
 
 app = FastAPI()
 app.add_middleware(
-    CORSMiddleware, allow_origins=["http://localhost:3000"],
+    CORSMiddleware, allow_origins=["http://localhost:3000", "https://www.leadifysolutions.xyz", "www.leadifysolutions.xyz", "leadifysolutions.xyz"],
     allow_credentials=True, allow_methods=['*'], allow_headers=['*'])
 
 @app.get("/")
