f9a613692a3ea3d8afe493193bec1adea3416ea8
Basil Benny <85282801+basilbenny1002@users.noreply.github.com>
2025-05-09 21:50:22 +0530

    Update main.py
    Add payment routes

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware, allow_origins=["*"],
     allow_credentials=True, allow_methods=['*'], allow_headers=['*'])
-
+app.include_router(webhook_router) '
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
