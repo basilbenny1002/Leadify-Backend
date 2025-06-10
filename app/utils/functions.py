4cef12fa22c0727c744ee37bb8a268dc4c464ac7
Ank <ankon495@gmail.com>
2025-06-10 15:27:22 +0600

    Refactor file path for category JSON to use os.path.join for better compatibility

--- app/utils/functions.py
+++ app/utils/functions.py
@@ -50,7 +50,7 @@ def load_config():
     load_dotenv(dotenv_path=Path(__file__).resolve().parent.parent.parent / '.env')
 
 def category_to_id(category: str):
-    with open(r".\app\utils\datas\categories.json", "r", encoding="utf-8") as f:
+    with open(f"{os.path.join('app', 'utils', 'datas', 'categories.json')}", "r", encoding="utf-8") as f:
         data = json.load(f)
         return int(data[category])
                   
